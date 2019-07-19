f5c463b041912980d647ee117b84ffd6528ce3ba
Ferhnir <zax1984@gmail.com>
2019-07-19 22:22:12 +0100

    Project init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <navbar />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,22 +8,30 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/navbar/navbar.vue'
 
 export default {
   name: 'app',
   components: {
+    Navbar,
     HelloWorld
   }
 }
 </script>
 
 <style>
+body {
+
+  padding: 0;
+  margin: 0;
+  background-color: #000 !important;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
